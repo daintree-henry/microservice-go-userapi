0606fe1c654c8992022f634d756ea7637e3a83a3
daintree-henry <daintee.henry@gmail.com>
2021-01-10 00:20:21 +0900

    20210110 application

--- app/application.go
+++ app/application.go
@@ -1,11 +1,24 @@
 package app
 
 import (
+	"os"
+
+	"github.com/daintree-henry/microservice-go-userapi/utils/logger"
 	"github.com/gin-gonic/gin"
 )
 
-var router = gin.Default()
+const envPort = "ENV_PORT"
+
+var (
+	port   = os.Getenv(envPort)
+	router = gin.Default()
+)
 
 func StartApplication() {
-	Router()
+	mapUrls()
+
+	logger.Info("starting application")
+	//router.Run(fmt.Sprintf(":", port))
+	//TODO: 배포 전 포트 수정 필요
+	router.Run(":8080")
 }
