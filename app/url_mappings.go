3532f363ed1745a67e6258f08716001e8b26fc8e
daintree-henry <daintee.henry@gmail.com>
2021-01-18 23:47:07 +0900

    add mapUrls

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,5 +1,22 @@
 package app
 
+import (
+	"github.com/daintree-henry/microservice-go-userapi/controller/health"
+	"github.com/daintree-henry/microservice-go-userapi/controller/users"
+)
+
 func mapUrls() {
+	router.GET("/health", health.Health)
+
+	router.POST("/users", users.CtrCreateUser)
+	router.GET("/users/:id", users.CtrGetUserById)
+	router.PUT("/users/:id", users.CtrUpdateUser)
+	router.PATCH("/users/:id", users.CtrUpdateUser)
+	router.DELETE("/users", users.CtrDeleteUserById)
+
+	router.POST("/login", users.CtrLoginUserByIdAndPw)
+
+	router.PUT("/search/phone", users.CtrFindIdByPhoneNumber)
+	router.GET("/search/status", users.CtrGetUsersByStatus)
 
 }
