efbb8bad64b7c6074b575d21f8a7efb6a8733559
daintree-henry <daintee.henry@gmail.com>
2021-01-18 23:46:42 +0900

    20210118 extend password size

--- DDO.sql
+++ DDO.sql
@@ -3,13 +3,13 @@ CREATE SCHEMA `userdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci 
 CREATE TABLE `userdb`.`users` (
   `primary_key` BIGINT NOT NULL AUTO_INCREMENT,
   `id` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(200) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `phone_number` VARCHAR(45) NOT NULL,
   `status` VARCHAR(45) NULL,
   `created_at` DATETIME NOT NULL,
-  `modified_at` DATETIME NOT NULL,
+  `modified_at` DATETIME,
   PRIMARY KEY (`primary_key`),
   UNIQUE INDEX `ID_UNIQUE` (`id` ASC) VISIBLE);
 
